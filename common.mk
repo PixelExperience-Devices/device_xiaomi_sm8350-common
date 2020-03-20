5aa313a7e67fce439e074838b4cda0218e208cee
Arian <arian.kulmer@web.de>
2020-03-20 23:42:25 +0100

    sm8350-common: parts: Import from xiaomi_sdm845-common
    https://github.com/LineageOS/android_device_xiaomi_sdm845-common/tree/e261035d91b34cf0fdf0f012927e06a2bcdb03ed/parts
    
    Change-Id: I442f48780ca020f3e420ee4ccaf8ac07b05457cb
    Signed-off-by: Chenyang Zhong <zhongcy95@gmail.com>

--- common.mk
+++ common.mk
@@ -156,6 +156,10 @@ PRODUCT_PACKAGES += \
     libcodec2_vndk.vendor \
     libcodec2_hidl@1.0.vendor
 
+# Device-specific settings
+PRODUCT_PACKAGES += \
+    XiaomiParts
+
 # Display
 PRODUCT_PACKAGES += \
     android.hardware.graphics.mapper@3.0-impl-qti-display \
