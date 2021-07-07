bb9d4d24c259a196ed9601865a7640ab92fbb82c
Siddharth Kapoor <ksiddharth@google.com>
2021-07-07 14:21:39 +0800

    sm8350-common: Move launcher prop from vendor to system
    Launcher prop needs to be accessed in system partition only,
    hence move the related prop to /system/build.prop
    
    Bug: 192621663
    Test: ro.launcher.blur.appLaunch prop is set correctly,
      and Blurs are disabled on App launch.
    Change-Id: I6721d59498c5d35dfd560f429b8aed7a01ac29fc
    Signed-off-by: Chenyang Zhong <zhongcy95@gmail.com>

--- common.mk
+++ common.mk
@@ -112,7 +112,9 @@ PRODUCT_COPY_FILES += \
 
 # Blur
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
-    ro.surface_flinger.supports_background_blur=1 \
+    ro.surface_flinger.supports_background_blur=1
+
+PRODUCT_SYSTEM_PROPERTIES += \
     ro.launcher.blur.appLaunch=0
 
 # Boot animation
