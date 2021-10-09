301cde9867517c15a3e187111975ad0d59604cf6
Chenyang Zhong <zhongcy95@gmail.com>
2021-10-09 11:30:34 -0400

    sm8350-common: use SYSTEM_EXT_{PUBLIC,PRIVATE}_SEPOLICY_DIRS
    BOARD_PLAT_PRIVATE_SEPOLICY_DIR and BOARD_PLAT_PUBLIC_SEPOLICY_DIR
    has been deprecated.
    
    Signed-off-by: Chenyang Zhong <zhongcy95@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -179,8 +179,8 @@ VENDOR_SECURITY_PATCH := 2021-10-01
 # Sepolicy
 include device/qcom/sepolicy_vndr/SEPolicy.mk
 
-BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(COMMON_PATH)/sepolicy/private
-BOARD_PLAT_PUBLIC_SEPOLICY_DIR += $(COMMON_PATH)/sepolicy/public
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/private
+SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/public
 BOARD_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/vendor
 
 # Soong
