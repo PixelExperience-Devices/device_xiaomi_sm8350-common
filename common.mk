a525c0de4c5bd9a9a3fc45b4992ec08575cdb80a
TH779 <i@779.moe>
2021-10-14 15:37:47 +0000

    sm8350-common: Uprev fingerprint HAL to 2.3
    Signed-off-by: TH779 <i@779.moe>
    Change-Id: Ia36a9f6f2ed866824a95c2763eae4da15293c448

--- common.mk
+++ common.mk
@@ -191,8 +191,9 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.1.vendor \
-    android.hardware.biometrics.fingerprint@2.1-service.xiaomi_sm8350
+    android.hardware.biometrics.fingerprint@2.3.vendor \
+    android.hardware.biometrics.fingerprint@2.3-service.xiaomi_sm8350 \
+    vendor.goodix.hardware.biometrics.fingerprint@2.1.vendor
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
