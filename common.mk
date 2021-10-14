8bf715623da683eb6561eb3fb0166de41648eb0f
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-10-14 19:15:50 +0200

    sm8350-common: Move to AOSP NXP NFC HAL
    * NXP added snxxx support in AOSP
    
    Change-Id: Id1b9cdbe1f29d1ff61af8182ae2f8370e5210426
    Signed-off-by: Sebastiano Barezzi <barezzisebastiano@gmail.com>

--- common.mk
+++ common.mk
@@ -333,23 +333,16 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.ipsec_tunnels.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.ipsec_tunnels.xml
 
 # NFC
-PRODUCT_SOONG_NAMESPACES += \
-    vendor/nxp/opensource/sn100x
-
 PRODUCT_PACKAGES += \
     com.android.nfc_extras \
-    com.nxp.nfc.nq \
-    NQNfcNci \
+    NfcNci \
+    SecureElement \
     Tag
 
 PRODUCT_PACKAGES += \
-    nfc_nci.nqx.default.hw \
-    vendor.nxp.hardware.nfc@2.0-service
-
-PRODUCT_PACKAGES += \
-    jcos_nq_client \
-    ls_nq_client \
-    se_nq_extn_client
+    android.hardware.nfc_snxxx@1.2-service \
+    android.hardware.secure_element@1.2.vendor \
+    libchrome.vendor
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.nfc.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.nfc.xml \
