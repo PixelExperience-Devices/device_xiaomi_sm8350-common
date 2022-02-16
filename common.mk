557d553b304b6d53801096d31bc97484f2332e05
Cosmin Tanislav <demonsingur@gmail.com>
2022-02-16 22:14:58 +0200

    sm8350-common: Add dummy sensors sub HAL
    Change-Id: Ie20ed775344990de3e16e2eefba1754d831027fb
    Signed-off-by: Chenyang Zhong <zhongcy95@gmail.com>

--- common.mk
+++ common.mk
@@ -454,6 +454,7 @@ PRODUCT_PACKAGES += \
 # Sensors
 PRODUCT_PACKAGES += \
     android.hardware.sensors@2.1-service.xiaomi_sm8350-multihal \
+    sensors.xiaomi_sm8350 \
     vendor.qti.hardware.display.mapper@1.1.vendor
 
 PRODUCT_COPY_FILES += \
