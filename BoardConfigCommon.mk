7ed77789b9532fe9b6c08e722f41b132fae65a0b
Chenyang Zhong <zhongcy95@gmail.com>
2022-02-20 06:44:02 -0500

    sm8350-common: Permissive SELinux
    Signed-off-by: Chenyang Zhong <zhongcy95@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -109,6 +109,7 @@ BOARD_KERNEL_CMDLINE += pcie_ports=compat
 BOARD_KERNEL_CMDLINE += iptable_raw.raw_before_defrag=1
 BOARD_KERNEL_CMDLINE += ip6table_raw.raw_before_defrag=1
 BOARD_KERNEL_CMDLINE += video=vfb:640x400,bpp=32,memsize=3072000
+BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 
 # Partitions
 BOARD_FLASH_BLOCK_SIZE := 131072
