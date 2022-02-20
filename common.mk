d902369f9ac2ea22947c0f9096455d35ef5d779a
Chenyang Zhong <zhongcy95@gmail.com>
2022-02-20 09:10:12 -0500

    sm8350-common: rework audio
    - audio.primary.lahaina is shipped as prebuilt
    - android.hardware.audio.service should be built from source
    - do not build the audio ext packages as they should be bundled as
      prebuilt with audio.primary.lahaina
    
    Signed-off-by: Chenyang Zhong <zhongcy95@gmail.com>

--- common.mk
+++ common.mk
@@ -52,13 +52,6 @@ PRODUCT_PACKAGES += \
     android.hardware.audio.service \
     android.hardware.soundtrigger@2.2-impl
 
-PRODUCT_PACKAGES += \
-    liba2dpoffload \
-    libbatterylistener \
-    libhdmiedid \
-    libhfp \
-    libsndmonitor
-
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_configs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_configs.xml \
     $(LOCAL_PATH)/audio/audio_tuning_mixer.txt:$(TARGET_COPY_OUT_VENDOR)/etc/audio_tuning_mixer.txt \
