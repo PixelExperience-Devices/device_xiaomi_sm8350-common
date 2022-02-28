a53b27ee4797f3d515ed0056ea5478fa56dd2a94
Chenyang Zhong <zhongcy95@gmail.com>
2022-02-28 08:51:57 -0500

    sm8350-common: drop overlay-lineage
    Signed-off-by: Chenyang Zhong <zhongcy95@gmail.com>

--- common.mk
+++ common.mk
@@ -381,8 +381,7 @@ PRODUCT_COPY_FILES += \
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
