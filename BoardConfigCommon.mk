1413b4a01246fefd325196c150d8aeaf83b2c4e6
EcrosoftXiao <xy19990903@gmail.com>
2022-03-13 14:42:11 +0800

    PE bringup

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -71,7 +71,7 @@ DEVICE_MATRIX_FILE := $(COMMON_PATH)/hidl/compatibility_matrix.xml
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     $(COMMON_PATH)/hidl/vendor_framework_compatibility_matrix.xml \
     $(COMMON_PATH)/hidl/xiaomi_framework_compatibility_matrix.xml \
-    vendor/lineage/config/device_framework_matrix.xml
+    vendor/aosp/config/device_framework_matrix.xml
 
 DEVICE_MANIFEST_FILE := \
     $(COMMON_PATH)/hidl/manifest_lahaina.xml \
