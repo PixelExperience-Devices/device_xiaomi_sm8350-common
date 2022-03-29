b042c893a5a496413f797352255f8788d22e2a2a
LuK1337 <priv.luk@gmail.com>
2022-03-30 00:30:07 +0200

    sm8350-common: Include vendor/debugfs.config
    This disables the following kernel configs:
    * CONFIG_DEBUG_FS
    * CONFIG_PAGE_OWNER
    
    Change-Id: Idbf74e3dee4dccff4708462271e79374e28449ae

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -84,7 +84,7 @@ BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOT_HEADER_VERSION)
 TARGET_KERNEL_ADDITIONAL_FLAGS := TARGET_PRODUCT=$(PRODUCT_DEVICE)
 TARGET_KERNEL_NO_GCC := true
 TARGET_KERNEL_SOURCE := kernel/xiaomi/sm8350
-TARGET_KERNEL_CONFIG := vendor/lahaina-qgki_defconfig vendor/xiaomi_QGKI.config
+TARGET_KERNEL_CONFIG := vendor/lahaina-qgki_defconfig vendor/debugfs.config vendor/xiaomi_QGKI.config
 
 BOARD_KERNEL_CMDLINE += androidboot.console=ttyMSM0
 BOARD_KERNEL_CMDLINE += androidboot.hardware=qcom
