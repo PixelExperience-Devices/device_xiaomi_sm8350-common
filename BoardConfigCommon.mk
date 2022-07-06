d2285ea4bc565168a0f2fcd2aac5d46d750c59c2
Chenyang Zhong <zhongcy95@gmail.com>
2022-07-06 10:35:52 -0400

    sm8350-common: drop DTC and LLVM flags for building kernel
    They are already set by default in vendor/aosp.
    
    Signed-off-by: Chenyang Zhong <zhongcy95@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -93,7 +93,6 @@ KERNEL_LD := LD=ld.lld
 BOARD_BOOT_HEADER_VERSION := 3
 BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOT_HEADER_VERSION)
 
-TARGET_KERNEL_ADDITIONAL_FLAGS := DTC_EXT=$(shell pwd)/prebuilts/misc/linux-x86/dtc/dtc LLVM=1
 TARGET_KERNEL_CLANG_COMPILE := true
 
 BOARD_KERNEL_CMDLINE += androidboot.console=ttyMSM0
