bcec086ba3e107b75827fc229c1d7c1557ee6384
Chenyang Zhong <zhongcy95@gmail.com>
2022-08-21 09:51:26 -0400

    sm8350-common: parts: set default doze brightness to HBM
    LBM is quite dim. It is barely visible in a normally-lit in-door
    environment, so use HBM as default instead. This is also inline with
    the default mode in kernel.
    
    Signed-off-by: Chenyang Zhong <zhongcy95@gmail.com>

--- parts/src/org/lineageos/settings/doze/DozeUtils.java
+++ parts/src/org/lineageos/settings/doze/DozeUtils.java
@@ -87,7 +87,7 @@ public final class DozeUtils {
     private static void restoreDozeModes(Context context) {
         if (!isDozeAutoBrightnessEnabled(context)) {
             setDozeMode(PreferenceManager.getDefaultSharedPreferences(context).getString(
-                    DOZE_BRIGHTNESS_KEY, String.valueOf(DOZE_BRIGHTNESS_LBM)));
+                    DOZE_BRIGHTNESS_KEY, String.valueOf(DOZE_MODE_HBM)));
         }
     }
 
@@ -135,7 +135,7 @@ public final class DozeUtils {
 
     protected static boolean isDozeAutoBrightnessEnabled(Context context) {
         return PreferenceManager.getDefaultSharedPreferences(context)
-                .getString(DOZE_BRIGHTNESS_KEY, DOZE_BRIGHTNESS_LBM)
+                .getString(DOZE_BRIGHTNESS_KEY, DOZE_BRIGHTNESS_HBM)
                 .equals(DOZE_BRIGHTNESS_AUTO);
     }
 
