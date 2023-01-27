cb12f0665aabcb68cc94cb5cb0c9318f2693c48a
Chenyang Zhong <zhongcy95@gmail.com>
2023-01-27 00:52:03 -0500

    sm8350-common: rootdir: avoid migrating foreground tasks to X1
    The Cortex-X1 core is quite power hungary. Let's only use it for
    top-app tasks. Avoid migrating foreground tasks to X1 to reduce
    unnecessary bumps of X1's frequency and save some battery.
    
    Signed-off-by: Chenyang Zhong <zhongcy95@gmail.com>

--- rootdir/bin/init.kernel.post_boot-lahaina.sh
+++ rootdir/bin/init.kernel.post_boot-lahaina.sh
@@ -189,6 +189,7 @@ echo 325 > /proc/sys/kernel/walt_low_latency_task_threshold
 # cpuset parameters
 echo 0-3 > /dev/cpuset/background/cpus
 echo 0-3 > /dev/cpuset/system-background/cpus
+echo 0-6 > /dev/cpuset/foreground/cpus
 
 # configure governor settings for silver cluster
 echo "schedutil" > /sys/devices/system/cpu/cpufreq/policy0/scaling_governor
