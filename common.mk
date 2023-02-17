fcdb6165005010f810e02327d8ace01d57a5bc87
FlowerSea0208 <zzp060208@gmail.com>
2023-02-17 22:28:54 +0800

    sm8350-common: Remove duplicate perf hal
    Change-Id: Ifb66cfd50a1bcb308d767d6b347dedd2800710f9

--- common.mk
+++ common.mk
@@ -424,7 +424,6 @@ PRODUCT_COPY_FILES += \
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power-service-qti \
-    vendor.qti.hardware.perf@2.2.vendor
 
 PRODUCT_COPY_FILES += \
     vendor/qcom/opensource/power/config/lahaina/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
