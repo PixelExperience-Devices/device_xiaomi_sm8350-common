7f3d4cc88de65138e2b4b5fb4e44696b6137489a
AdarshGrewal <adarshgrewal@gmail.com>
2023-03-05 13:08:37 +0530

    sm8350-common: Update from haydn V14.0.3.0.TKKMIXM
    Change-Id: I319efc1d4ae76192c8314b37b65db72cdfb1ba04

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -173,7 +173,7 @@ TARGET_USERIMAGES_USE_F2FS := true
 ENABLE_VENDOR_RIL_SERVICE := true
 
 # Security patch level
-VENDOR_SECURITY_PATCH := 2022-11-01
+VENDOR_SECURITY_PATCH := 2023-01-01
 
 # Sepolicy
 include device/qcom/sepolicy_vndr-legacy-um/SEPolicy.mk
