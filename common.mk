f9363b11e2515df763c3c76a7d0d2fac011c9802
Arian <arian.kulmer@web.de>
2023-03-24 14:40:52 +0100

    sm8350-common: Import public.libraries.txt from haydn V14.0.3.0.TKKMIXM
    Change-Id: Ifcd7c40246b8b43d52d151001441bfd09b80f24e

--- common.mk
+++ common.mk
@@ -334,6 +334,10 @@ PRODUCT_PACKAGES += \
     libavservices_minijail \
     libavservices_minijail.vendor
 
+# Native libraries whitelist
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
+
 # Network
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.ipsec_tunnels.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.ipsec_tunnels.xml
